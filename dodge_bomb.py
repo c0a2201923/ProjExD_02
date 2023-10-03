b74656fb953f4076d2ad8fe3d70724fcaf760717
Hiroki <c0a2201923@edu.teu.ac.jp>
2023-10-03 15:28:31 +0900

    練習４

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,6 +12,19 @@ delta = {  #練習３ : 移動量辞書
     pg.K_RIGHT: (+5, 0),
 }
 
+def check_bound(obj_rct: pg.Rect):
+    """
+    引数 : こうかとんRectかばくだんRect
+    戻り値 : タプル (横方向判断結果, 縦方向判定結果)
+    画面内ならTrue, 画面外ならFalse
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:  # 練習４ : 横方向判定
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:  #練習４ : 縦方向判定
+        tate = False
+    return yoko, tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -21,6 +34,7 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     kk_rct = kk_img.get_rect()
+    
     kk_rct.center = (900, 400)  #練習３ : こうかとんの初期座標を設定する
 
     """ばくだん"""
@@ -30,8 +44,8 @@ def main():
     bd_rct = bd_img.get_rect() # 練習１ : SurfaceからRectを抽出する
     x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT) 
     bd_rct.center = (x, y)  # 練習１ : Rectにランダム名座標を設定する
-    clock = pg.time.Clock()
     vx, vy = +5, +5  # 練習２ : 爆弾の速度
+    clock = pg.time.Clock()
 
 
     tmr = 0
@@ -50,14 +64,21 @@ def main():
                 sum_mv[0] += mv[0]  # 練習３ : 横方向の合計移動量
                 sum_mv[1] += mv[1]  # 練習３ : 縦方向の合計移動量
         kk_rct.move_ip(sum_mv[0], sum_mv[1])
-        screen.blit(kk_img, kk_rct)  #練習３ : 移動後の座標に表示させる
+        if check_bound(kk_rct) != (True, True):  #練習４ : はみ出し判定
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        screen.blit(kk_img, kk_rct)  # 練習３ : 移動後の座標に表示させる
 
-        """ばくだん"""
+        """ばくだん""" 
         bd_rct.move_ip(vx, vy)  # 練習２ : 爆弾を移動させる
+        yoko, tate = check_bound(bd_rct)
+        if not yoko:  # 練習４ : 横方向にはみ出たら
+            vx *= -1
+        if not tate:  # 練習４ : 縦方向にはみ出たら
+            vy *= -1
         screen.blit(bd_img, bd_rct)  # 練習１ : Rectを使って試しにblit
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
